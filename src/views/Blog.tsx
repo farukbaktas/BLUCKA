bec2677e603405249f70e65a8213b40b6efa9d77
Faruk Baktas <70661808+farukbaktas@users.noreply.github.com>
2023-12-07 12:32:58 +0300

    Update Blog.tsx

--- src/views/Blog.tsx
+++ src/views/Blog.tsx
@@ -20,24 +20,8 @@ const Blog = () => {
 
 
             <div className="grid gap-8 lg:grid-cols-3  item-center justify-center">
-            <Link to="/blog/merging-genius-for-web3-marketing">
-                    <div className="max-w-sm bg-white border-0.5 border-gray-200 rounded-lg shadow">
-                        <img className="rounded-t-lg" src="https://media.discordapp.net/attachments/1171396831989739570/1181879048977272852/1_ZJer_ze2P6vAdghx8aNvMw.webp?ex=6582a9a3&is=657034a3&hm=49ec7bf0891c41ad3b0ee12884eee831349225a72cce702c80716e229ea18571&=&format=webp&width=2268&height=1064" alt="Thumbnail" />
-                        <div className="p-5">
-                            
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Merging Genius for Web3 Marketing</h5>
-                            
-                            <p className="mb-3 font-normal text-gray-700">Introducing BLUCKA: The Crypto Creative Agency, a trailblazing entity at the forefront of Web3 marketing.</p>
-                            <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                                Read more
-                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>  
-                </Link>
-                <Link to="/blog/elevating-presence-in-bull-market">
+
+            <Link to="/blog/elevating-presence-in-bull-market">
                     <div className="max-w-sm bg-white border-0.5 border-gray-200 rounded-lg shadow">
                         <img className="rounded-t-lg" src="https://cdn.discordapp.com/attachments/1171396831989739570/1181579356477345865/1_TJYX4NG3o9kTzthWBqBF7Q.webp?ex=65819287&is=656f1d87&hm=8ca3a8f849f294684b76ac21212c51a8accbf9c7095b792d378c8d4556b2b33a&" alt="Thumbnail" />
                         <div className="p-5">
@@ -55,9 +39,24 @@ const Blog = () => {
                     </div>  
                 </Link>
 
-                
 
-
+                  <Link to="/blog/merging-genius-for-web3-marketing">
+                    <div className="max-w-sm bg-white border-0.5 border-gray-200 rounded-lg shadow">
+                        <img className="rounded-t-lg" src="https://media.discordapp.net/attachments/1171396831989739570/1181879048977272852/1_ZJer_ze2P6vAdghx8aNvMw.webp?ex=6582a9a3&is=657034a3&hm=49ec7bf0891c41ad3b0ee12884eee831349225a72cce702c80716e229ea18571&=&format=webp&width=2268&height=1064" alt="Thumbnail" />
+                        <div className="p-5">
+                            
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Merging Genius for Web3 Marketing</h5>
+                            
+                            <p className="mb-3 font-normal text-gray-700">Introducing BLUCKA: The Crypto Creative Agency, a trailblazing entity at the forefront of Web3 marketing.</p>
+                            <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                Read more
+                                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>  
+                </Link>
                 <Link to="/blog/unleash-the-web3-gaming">
                     <div className="max-w-sm bg-white border-0.5 border-gray-200 rounded-lg shadow">
                         <img className="rounded-t-lg" src="https://cdn.discordapp.com/attachments/1171396831989739570/1182228492004376697/1_wTgpP45vRW4uIHCJDqNzGg.webp?ex=6583ef15&is=65717a15&hm=477cc0f6fab01cc002440c5ccea0100d9f148a02cc4322e1df08c68ec2d3f0ae&" alt="Thumbnail" />
@@ -74,7 +73,7 @@ const Blog = () => {
                             </div>
                         </div>
                     </div>  
-                </Link>  
+                </Link>
             </div> 
              
         </div>
