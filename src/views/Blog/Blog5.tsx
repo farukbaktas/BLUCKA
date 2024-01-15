f4e30ad4659289ca15d672c42058622c85c78a0e
Faruk Baktas <70661808+farukbaktas@users.noreply.github.com>
2024-01-15 16:04:19 +0300

    Update Blog5.tsx

--- src/views/Blog/Blog5.tsx
+++ src/views/Blog/Blog5.tsx
@@ -17,7 +17,7 @@ const Blog = () => {
         {/* Open Graph for Facebook and LinkedIn */}
         <meta property="og:title" content="Give Me Liquidity, or Give Me Death" />
         <meta property="og:description" content="In a dystopian world, Bitcoin liquidity is now dictating every aspect of life." />
-        <meta property="og:image" content="https://media.discordapp.net/attachments/1171396831989739570/1192832511391309915/MARSHALPPF.jpg?ex=65aa82d8&is=65980dd8&hm=95623294ceb1dff21b12191437962297ee785664cab579281cb276937b896b0e&=&format=webp&width=1228&height=1228" />
+        <meta property="og:image" content="https://media.discordapp.net/attachments/1171396831989739570/1195718876525252659/Larticle.jpg?ex=65b502fb&is=65a28dfb&hm=f3351545d9aa2d042e41198e9ab92cebd18b497610f8c24439bc26e02305a3dc&=&format=webp&width=2268&height=1064" />
         <meta property="og:url" content="https://www.blucka.com/blog/give-me-liquidity-or-give-me-death" />
         <meta property="og:type" content="website" />
 
@@ -25,8 +25,7 @@ const Blog = () => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Give Me Liquidity, or Give Me Death" />
         <meta name="twitter:description" content="In a dystopian world, Bitcoin liquidity is now dictating every aspect of life." />
-        <meta name="twitter:image" content="https://media.discordapp.net/attachments/1171396831989739570/1192832511391309915/MARSHALPPF.jpg?ex=65aa82d8&is=65980dd8&hm=95623294ceb1dff21b12191437962297ee785664cab579281cb276937b896b0e&=&format=webp&width=1228&height=1228" />
-
+        <meta name="twitter:image" content="https://media.discordapp.net/attachments/1171396831989739570/1195718876525252659/Larticle.jpg?ex=65b502fb&is=65a28dfb&hm=f3351545d9aa2d042e41198e9ab92cebd18b497610f8c24439bc26e02305a3dc&=&format=webp&width=2268&height=1064" />
     </Helmet>
     <div className="font-mono cursor bg-cover text-black w-screen relative">
         <div className="relative">
