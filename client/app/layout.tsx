f4bf1fc73ff6802b39da212ff20fe01cfb4e4f13
Faruk Baktas <70661808+farukbaktas@users.noreply.github.com>
2024-04-01 13:11:59 +0300

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -18,6 +18,21 @@ export default function RootLayout({
   console.log("RootLayout");
   return (
     <html lang="en">
+      <head>
+          {/* Google Analytics*/}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-2GS59HQ3YH"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-2GS59HQ3YH');
+              `,
+            }}
+          />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
