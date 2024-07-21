8c73a3ad531615d20695022b335fae3c781b61e4
Faruk Baktas <70661808+farukbaktas@users.noreply.github.com>
2024-07-21 16:44:48 +0300

    Sitemap

--- client/next-sitemap.js
+++ client/next-sitemap.js
@@ -1,5 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://blucka.com',
-    generateRobotsTxt: true,
-  }
+    siteUrl: 'https://www.blucka.com', // Change this to your site's URL
+    generateRobotsTxt: true, // Generate a robots.txt file
+  };
   
